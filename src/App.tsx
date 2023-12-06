0a38541870fea21cd972d1adc8a36fbb5694ba9f
Tony.Mukesh <tony.mukesh@rubicotech.in>
2023-12-06 16:19:39 +0530

    feat: initial codebase setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(): React.ReactNode {
+  return (
+    <div>
+      <h2 className="bg-black text-white text-center">Hello World</h2>
+    </div>
+  );
+}
